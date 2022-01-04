8365a6ff0ddf06011337b73ae862fa2836aa973e
varunmehta14 <mehtavarunj@gmail.com>
2022-01-04 18:14:41 +0530

    voice

--- ESahayak-frontend/android/app/src/main/java/com/esahayak/MainApplication.java
+++ ESahayak-frontend/android/app/src/main/java/com/esahayak/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.wenkesj.voice.VoicePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,7 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage; // <- add this
 import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add this
-
+// import com.wenkesj.voice.VoicePackage; // <------ Add this!
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          // packages.add(new VoicePackage());
           return packages;
         }
 
